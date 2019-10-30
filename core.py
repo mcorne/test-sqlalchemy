93f6f0c197d095d06a46c5ab9721d57724305eb8
Michel Corne <mcorne@yahoo.com>
2019-10-30 10:10:57 +0100

    added multiple row insert

--- core.py
+++ core.py
@@ -3,6 +3,7 @@ from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
 
 engine = create_engine("sqlite:///database.sqlite3", echo=True)
 metadata = MetaData()
+
 users = Table(
     "users",
     metadata,
@@ -11,13 +12,35 @@ users = Table(
     Column("fullname", String),
 )
 
-users.drop(engine, checkfirst=True)
-users.create(engine, checkfirst=True)
-# metadata.create_all(checkfirst=True)
+addresses = Table(
+    "addresses",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("user_id", None, ForeignKey("users.id")),
+    Column("email_address", String, nullable=False),
+)
 
-ins = users.insert().values(name="jack", fullname="Jack Jones")
-print(str(ins))
-print(ins.compile().params)
+# users.drop(engine, checkfirst=True)
+# users.create(engine, checkfirst=True)
+metadata.drop_all(engine, checkfirst=True)
+metadata.create_all(engine, checkfirst=True)
 
 conn = engine.connect()
+ins = users.insert().values(name="jack", fullname="Jack Jones")
+# print(str(ins))
+# print(ins.compile().params)
 result = conn.execute(ins)
+# print(result.inserted_primary_key)
+ins = users.insert()
+conn.execute(ins, id=2, name="wendy", fullname="Wendy Williams")
+
+conn.execute(
+    addresses.insert(),
+    [
+        {"user_id": 1, "email_address": "jack@yahoo.com"},
+        {"user_id": 1, "email_address": "jack@msn.com"},
+        {"user_id": 2, "email_address": "www@www.org"},
+        {"user_id": 2, "email_address": "wendy@aol.com"},
+    ],
+)
+

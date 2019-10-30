6de449690b590aed5f4157d2cc17620b426b60fb
Michel Corne <mcorne@yahoo.com>
2019-10-30 10:39:11 +0100

    added select

--- core.py
+++ core.py
@@ -1,5 +1,13 @@
-from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy import (
+    Column,
+    ForeignKey,
+    Integer,
+    MetaData,
+    String,
+    Table,
+    create_engine,
+)
+from sqlalchemy.sql import select
 
 engine = create_engine("sqlite:///database.sqlite3", echo=True)
 metadata = MetaData()
@@ -26,11 +34,13 @@ metadata.drop_all(engine, checkfirst=True)
 metadata.create_all(engine, checkfirst=True)
 
 conn = engine.connect()
+
 ins = users.insert().values(name="jack", fullname="Jack Jones")
-# print(str(ins))
-# print(ins.compile().params)
+print(str(ins))
+print(ins.compile().params)
 result = conn.execute(ins)
-# print(result.inserted_primary_key)
+print(result.inserted_primary_key)
+
 ins = users.insert()
 conn.execute(ins, id=2, name="wendy", fullname="Wendy Williams")
 
@@ -44,3 +54,26 @@ conn.execute(
     ],
 )
 
+s = select([users])
+result = conn.execute(s)
+print(result)
+for row in result:
+    print(row)
+
+result = conn.execute(s)
+row = result.fetchone()
+print("name:", row["name"], "; fullname:", row["fullname"])
+
+row = result.fetchone()
+print("name:", row[1], "; fullname:", row[2])
+
+for row in conn.execute(s):
+    print("name:", row[users.c.name], "; fullname:", row[users.c.fullname])
+
+# result sets with pending rows remaining should be explicitly closed for some database API
+result.close()
+
+s = select([users.c.name, users.c.fullname])
+result = conn.execute(s)
+for row in result:
+    print(row)

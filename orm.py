48b8e1e959e55edbfae0d8b14224ce6c63e5bbfd
Michel Corne <mcorne@yahoo.com>
2019-11-06 15:19:51 +0100

    added relationship

--- orm.py
+++ orm.py
@@ -17,6 +17,7 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True)
     name = Column(String)
     fullname = Column(String)
@@ -28,6 +29,22 @@ class User(Base):
             self.fullname,
             self.nickname,
         )
+
+
+class Address(Base):
+    __tablename__ = "addresses"
+
+    id = Column(Integer, primary_key=True)
+    email_address = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    user = relationship("User", back_populates="addresses")
+
+    def __repr__(self):
+        return "<Address(email_address='%s')>" % self.email_address
+
+
+User.addresses = relationship("Address", order_by=Address.id, back_populates="user")
 
 
 engine = create_engine("sqlite:///orm.sqlite3", echo=True)
@@ -215,3 +232,28 @@ print("----------------------------------------")
 print("Counting group")
 print("----------------------------------------")
 print(session.query(func.count(User.name), User.name).group_by(User.name).all())
+
+print("----------------------------------------")
+print("Add user addresses")
+print("----------------------------------------")
+jack = User(name='jack', fullname='Jack Bean', nickname='gjffdd')
+jack.addresses = [
+    Address(email_address='jack@google.com'),
+    Address(email_address='j25@yahoo.com')]
+session.add(jack)
+session.commit()
+
+print("----------------------------------------")
+print("Query user addresses")
+print("----------------------------------------")
+jack = session.query(User).filter_by(name='jack').one()
+print(jack)
+print("----------------------------------------")
+print(jack.addresses)
+
+print("----------------------------------------")
+print("Query both users and addresses")
+print("----------------------------------------")
+for u, a in session.query(User, Address).filter(User.id==Address.user_id).filter(Address.email_address=='jack@google.com').all():
+    print(u)
+    print(a)
